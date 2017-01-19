3b0ea56a375f42e719220ff2ed46e4e81423fdd0
Dan Joseph <danbjoseph@gmail.com>
2017-01-19 15:14:11 -0500

    changing a setting so i dont need to remember when deploying

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ var settings = {}
 settings.app = {
   defaultpass: '123',
   defaultuser: 'me',
-	port: 3000,
+	port: 3002,
 	db: 'site.db',
   s3bucket: 'ifrc-vca'
   //Amazon S3 Credentials are stored in ~/.aws/credentials (see: http://aws.amazon.com/sdk-for-node-js/)
